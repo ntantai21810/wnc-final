a0058d79f2eb68a596f15eb608dc7807ae1960d1
nguyentai21 <nguyentantai9a3@gmail.com>
2023-01-05 23:41:39 +0700

    add socket

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthProvider from "./provider/auth-provider";
 import routes from "./routes";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { useAppDispatch } from "./hooks/redux";
+import { openNotification } from "./redux/notificationSlice";
 
 function App() {
+  const dispatch = useAppDispatch();
+
   const router = createBrowserRouter(
     routes.map((item) => ({
       path: item.path,
@@ -13,6 +19,26 @@ function App() {
       ),
     }))
   );
+
+  useEffect(() => {
+    const hubConnectionBuilder = new HubConnectionBuilder()
+      .withUrl("https://bankmaia.herokuapp.com/notification")
+      .configureLogging(LogLevel.Information)
+      .build();
+
+    hubConnectionBuilder
+      .start()
+      .then(() => console.log("Connect ws ok"))
+      .catch((err) => console.log(err));
+
+    hubConnectionBuilder.on(
+      "SendNotificationToUser",
+      (result: { id: number; description: string; time: string }) => {
+        console.log({ result });
+        dispatch(openNotification({ message: result.description }));
+      }
+    );
+  }, [dispatch]);
 
   return <RouterProvider router={router} />;
 }
