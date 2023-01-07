7816457c76f71a69bcd036af4ea6fae31f31a6c5
nguyentai21 <nguyentantai9a3@gmail.com>
2023-01-07 12:52:45 +0700

    update docker file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf docker-compose.yaml
-wget https://raw.githubusercontent.com/ntantai21810/demo-github-actions/master/docker-compose.yaml
+wget https://raw.githubusercontent.com/ntantai21810/wnc-final/master/docker-compose.yaml
 docker compose stop
 docker compose rm -f
 docker compose pull
